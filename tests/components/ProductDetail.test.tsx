c59666e8291f756e9760e080a46f2720c6403a79
fatimampg <fatimampgouveia@gmail.com>
2024-06-29 18:41:18 +0100

    tests: add mswjs/data and faker and update ProductDetail and ProductList tests

--- tests/components/ProductDetail.test.tsx
+++ tests/components/ProductDetail.test.tsx
@@ -1,18 +1,30 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitForElementToBeRemoved } from '@testing-library/react';
 import ProductDetail from '../../src/components/ProductDetail';
-import { products } from "../mocks/data";
-import { http, HttpResponse } from 'msw';
+// import { products } from "../mocks/data";
+import { http, HttpResponse, delay } from 'msw';
 import { server } from "../mocks/server";
+import { db } from '../mocks/db';
 
 describe('ProductDetail', () => {
-    it('should render the list of products', async () => {
-        render(<ProductDetail productId={1} />);
+    let productId: number;
 
-        expect(await screen.findByText(new RegExp(products[0].name))).toBeInTheDocument();
-        expect(await screen.findByText(new RegExp(products[0].price.toString()))).toBeInTheDocument();
+    beforeAll(() => {
+        const product = db.product.create();
+        productId = product.id;
+    });
+    afterAll(() => {
+        db.product.delete({ where: { id: { equals: productId } } })
+    });
+
+    it('should render product details', async () => {
+        const product = db.product.findFirst({ where: { id: { equals: productId } } });
+        render(<ProductDetail productId={productId} />);
+
+        expect(await screen.findByText(new RegExp(product!.name))).toBeInTheDocument();
+        expect(await screen.findByText(new RegExp(product!.price.toString()))).toBeInTheDocument();
     })
 
-    it('should render a message if there is no product', async () => {
+    it('should render message "not found" if there are no product', async () => {
         server.use(http.get('/products/1', () => HttpResponse.json(null)));
         render(<ProductDetail productId={1} />);
 
@@ -26,4 +38,38 @@ describe('ProductDetail', () => {
         const message = await screen.findByText(/invalid/i);
         expect(message).toBeInTheDocument();
     })
+
+    it('should render error message if data fetching fails', async () => {
+        server.use(http.get('/products/1', () => HttpResponse.error()));
+        render(<ProductDetail productId={1} />);
+       
+        expect(await screen.findByText(/error/i)).toBeInTheDocument();
+    })
+
+    it('should render a loading indicator when fetching data', async () => {
+        server.use(http.get('/products/1', async () => {
+            await delay();
+            return HttpResponse.json([]);
+        }));
+
+        render(<ProductDetail productId={1} />);
+
+        expect(await screen.findByText(/loading/i)).toBeInTheDocument();
+    });
+
+    it('should remove the loading indicator after fetching data', async () => {
+        render(<ProductDetail productId={1} />);
+
+        await waitForElementToBeRemoved(() => screen.queryByText(/loading/i))
+    });
+
+    it('should remove the loading indicator if data fetching fails', async () => {
+        server.use(http.get('/products/1', () => HttpResponse.error()));
+
+        render(<ProductDetail productId={1} />);
+
+        await waitForElementToBeRemoved(() => screen.queryByText(/loading/i))
+    });
 })
+
+
---
+++ tests/mocks/db.ts
@@ -0,0 +1,13 @@
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/unbound-method */
+import { factory, primaryKey } from '@mswjs/data';
+import { faker } from '@faker-js/faker';
+
+export const db = factory({
+    product: {
+        id: primaryKey(faker.number.int),
+        name: faker.commerce.productName,
+        price: () => faker.number.int({ min: 1, max: 100 }),
+    }
+});
